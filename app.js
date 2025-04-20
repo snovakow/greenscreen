aedeafaef540ee674594968fd8ab71621f8a2dbc
snovakow <snovakow@gmail.com>
2025-04-20 16:04:34 -0400

    0.0.3

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import * as THREE from 'three';
-import { Vector2, Vector3, Vector4 } from 'three';
+import {
+	Vector2, Color, ShaderMaterial, Mesh, Scene, OrthographicCamera, WebGLRenderer,
+	PlaneGeometry, BackSide, FrontSide, VideoTexture, SRGBColorSpace, LinearFilter,
+} from 'three';
 
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
@@ -21,11 +23,10 @@ const fragmentShader = `
 // From https://github.com/obsproject/obs-studio/blob/master/plugins/obs-filters/data/chroma_key_filter.effect
 
 uniform sampler2D uSampler;
-uniform sampler2D cropMap;
 
-// uniform float u_saturation;
+uniform float u_saturation;
 
-uniform vec4 color;
+uniform vec3 color;
 uniform float contrast;
 uniform float brightness;
 uniform float gamma;
@@ -55,24 +56,24 @@ float GetChromaDist(in vec3 rgb) {
 }
 
 // From https://thebookofshaders.com/06/
-// const vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-// vec3 rgb2hsb(in vec3 c) {
-// 	vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-// 	vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-// 	float d = q.x - min(q.w, q.y);
-// 	float e = 1.0e-10;
-// 	return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
-// }
-// vec3 hsb2rgb(in vec3 c) {
-// 	vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0);
-// 	rgb = rgb*rgb*(3.0-2.0*rgb);
-// 	return c.z * mix(vec3(1.0), rgb, c.y);
-// }
-// void changeSaturation(inout vec3 rgb, float change) {
-// 	vec3 hsb = rgb2hsb(rgb);
-// 	hsb.g *= change;
-// 	rgb = hsb2rgb(hsb);
-// }
+const vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+vec3 rgb2hsb(in vec3 c) {
+	vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+	vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+	float d = q.x - min(q.w, q.y);
+	float e = 1.0e-10;
+	return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+vec3 hsb2rgb(in vec3 c) {
+	vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0);
+	rgb = rgb*rgb*(3.0-2.0*rgb);
+	return c.z * mix(vec3(1.0), rgb, c.y);
+}
+void changeSaturation(inout vec3 rgb, float change) {
+	vec3 hsb = rgb2hsb(rgb);
+	hsb.g *= change;
+	rgb = hsb2rgb(hsb);
+}
 
 float GetBoxFilteredChromaDist(in vec3 rgb, in vec2 texCoord) {
 	vec2 h_pixel_size = pixelSize / 2.0;
@@ -89,7 +90,7 @@ float GetBoxFilteredChromaDist(in vec3 rgb, in vec2 texCoord) {
 }
 
 void main(void) {
-	float a = texture2D(cropMap, vTextureCoord).r * color.a;
+	float a = 1.0;
 
 	vec3 rgb = texture2D(uSampler, vTextureCoord).rgb;
 	float chromaDist = GetBoxFilteredChromaDist(rgb, vTextureCoord);
@@ -102,7 +103,7 @@ void main(void) {
 
 	float desat = clamp(rgb.r * 0.2126 + rgb.g * 0.7152 + rgb.b * 0.0722, 0.0, 1.0);
 
-	// if(u_saturation != 1.0) changeSaturation(rgb, u_saturation);
+	if(u_saturation != 1.0) changeSaturation(rgb, u_saturation);
 
 	float spillVal = pow(clamp(baseMask / u_spill, 0.0, 1.0), 1.5);
 	rgb = mix(vec3(desat, desat, desat), rgb, spillVal);
@@ -111,39 +112,40 @@ void main(void) {
 }
 `;
 
+const chromakey = new Color().setHex(0x00ff00);
+
 const uniforms = {
 	pixelSize: { value: new Vector2(1, 1) },
-	color: { value: new Vector4(1, 1, 1, 1) },
+	color: { value: new Color(0xffffff) },
 	contrast: { value: 1 },
 	brightness: { value: 0 },
 	gamma: { value: 1 },
-	chromakey: { value: new Vector2(...RGBtoUV(0, 1, 0)) },
+	chromakey: { value: new Vector2(...RGBtoUV(chromakey.r, chromakey.g, chromakey.b)) },
 	u_similarity: { value: 0.4 },
 	u_smoothness: { value: 0.08 },
 	u_spill: { value: 0.1 },
-	// u_saturation: { value: 1 },
+	u_saturation: { value: 1 },
 	uSampler: { value: null },
-	cropMap: { value: null }
 };
-const material = new THREE.ShaderMaterial({
+const material = new ShaderMaterial({
 	uniforms: uniforms,
 	vertexShader: vertexShaderString,
 	fragmentShader: fragmentShader
 });
 
-const geometry = new THREE.PlaneGeometry(1, 1);
-const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0x000000 }));
-mesh.material.side = THREE.FrontSide;
+const geometry = new PlaneGeometry(1, 1);
+const mesh = new Mesh(geometry, material);
+mesh.material.side = FrontSide;
 const degToRad = Math.PI / 180;
 const radToDeg = 180 / Math.PI;
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000);
+const scene = new Scene();
+scene.background = new Color(0x000000);
 scene.add(mesh);
 
-const camera = new THREE.OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0.5, -0.5);
+const camera = new OrthographicCamera(-0.5, 0.5, 0.5, -0.5, 0.5, -0.5);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -159,6 +161,13 @@ let videoLoaded = false;
 const video = document.createElement("video");
 video.autoplay = true;
 video.playsinline = true;
+
+video.style.position = 'fixed';
+video.style.bottom = '0px';
+video.style.right = '0px';
+video.style.width = '160px';
+document.body.appendChild(video);
+
 const resizeVideo = () => {
 	const windowAR = window.innerWidth / window.innerHeight;
 	const videoAR = video.videoWidth / video.videoHeight;
@@ -167,16 +176,19 @@ const resizeVideo = () => {
 	} else {
 		mesh.scale.set(videoAR / windowAR, 1, 1);
 	}
+
+	const pixSize = material.uniforms.pixelSize.value;
+	pixSize.x = 1 / renderer.domElement.width;
+	pixSize.y = 1 / renderer.domElement.height;
 }
 video.addEventListener("loadedmetadata", () => {
 	videoLoaded = true;
-	mesh.material.color.set(0xffffff);
+	// mesh.material.color.set(0xffffff);
 	resizeVideo();
 });
 
 const windowResize = () => {
 	renderer.setSize(window.innerWidth, window.innerHeight);
-
 	if (videoLoaded) resizeVideo();
 }
 window.addEventListener('resize', windowResize);
@@ -198,24 +210,34 @@ renderer.setAnimationLoop(animate);
 const gui = new GUI();
 
 const settings = {
-	flip: false
+	flip: false,
+	color: material.uniforms.color.value.getHex(),
+	chromakey: chromakey.getHex(),
 };
-gui.add(settings, 'flip').onChange((state) => {
+gui.add(settings, 'flip').name("Flip").onChange((state) => {
 	if (state) {
 		mesh.rotation.y = 180 * degToRad;
-		mesh.material.side = THREE.BackSide;
+		mesh.material.side = BackSide;
 	} else {
 		mesh.rotation.y = 0;
-		mesh.material.side = THREE.FrontSide;
+		mesh.material.side = FrontSide;
 	}
 });
+gui.addColor(settings, 'color').name("Color").onChange((color) => {
+	material.uniforms.color.value.setHex(color);
+});
 
-// gui.add(Env.light.shadow, 'radius', 0, 10).name('shadow radius');
-// gui.add(Env.light.shadow, 'normalBias', 0, 0.2).name('shadow normalBias');
-
-// gui.add(Env.sky.material.uniforms.turbidity, 'value', 0, 20).name('turbidity').onChange(() => {
-// 	Env.cubeCamera.update(Env.renderer, Env.skyScene);
-// });
+gui.add(material.uniforms.contrast, 'value', 0, 2).name('Contrast');
+gui.add(material.uniforms.brightness, 'value', -1, 1).name('Brightness');
+gui.add(material.uniforms.gamma, 'value', 1, 2.2).name('Gamma');
+gui.addColor(settings, 'chromakey').onChange((color) => {
+	chromakey.setHex(color);
+	material.uniforms.chromakey.value.set(...RGBtoUV(chromakey.r, chromakey.g, chromakey.b));
+});
+gui.add(material.uniforms.u_similarity, 'value', 0, 1).name('Similarity');
+gui.add(material.uniforms.u_smoothness, 'value', 0, 1).name('Smoothness');
+gui.add(material.uniforms.u_spill, 'value', 0, 1).name('Spill');
+gui.add(material.uniforms.u_saturation, 'value', 0, 2).name('Saturation');
 
 // const shadowMapTypes = {
 // 	BasicShadowMap: THREE.BasicShadowMap,
@@ -226,7 +248,6 @@ gui.add(settings, 'flip').onChange((state) => {
 // gui.add(Env.renderer.shadowMap, 'type', shadowMapTypes).name('shadowMap').onChange = () => {
 // 	Env.renderer.shadowMap.needsUpdate = true;
 // };
-// gui.add(Env.light.shadow, 'blurSamples', 1, 30).step(1).name('blurSamples');
 
 if (!navigator.mediaDevices?.enumerateDevices) {
 	console.log("enumerateDevices() not supported.");
@@ -250,34 +271,30 @@ async function getMedia(constraints) {
 	try {
 		stream = await navigator.mediaDevices.getUserMedia(constraints);
 		video.srcObject = stream;
-		//height: videoSettings.height,
-		//width: videoSettings.width,
 		video.play();
 
-		//document.body.appendChild(video);
-		const texture = new THREE.VideoTexture(video);
-		texture.colorSpace = THREE.SRGBColorSpace;
-		texture.minFilter = THREE.LinearFilter;
+		const texture = new VideoTexture(video);
+		texture.colorSpace = SRGBColorSpace;
+		texture.minFilter = LinearFilter;
 		if (mesh.material.map) mesh.material.map.dispose();
 		mesh.material.map = texture;
 		mesh.material.needsUpdate = true;
-		// uniforms.uSampler.value = texture;
+		uniforms.uSampler.value = texture;
 	} catch (err) {
 		console.log(err);
-		/* handle the error */
 	}
 }
 
 const constraints = { audio: false, video: true };
 const button = document.createElement('button');
-button.textContent = "Preview";
+button.textContent = "Load";
 button.onclick = () => {
 	videoLoaded = false;
 	getMedia(constraints);
 }
 button.style.position = 'absolute';
-button.style.top = '0px';
-button.style.left = '0px';
-getMedia(constraints);
-
+button.style.bottom = '0px';
+button.style.right = '0px';
 document.body.appendChild(button);
+
+getMedia(constraints);
